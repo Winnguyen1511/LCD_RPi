0c281895fe856333d448ac8cf24c66892bbdf690
Winnguyen <khoanguyen1507dn@gmail.com>
2020-02-06 12:10:22 +0700

    debug

--- src/testLCD.c
+++ src/testLCD.c
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
     I2C_Init(&i2c, 1);
     lcd_init(&i2c);
     sleep(2);
-    lcd_goto_XY(&i2c, 1,0);
+    lcd_goto_XY(&i2c, 2,0);
     lcd_send_string(&i2c, "hello world");
     
     return SUCCESS;
